6ab2df3f85d2d5dfa7f8fab6ee9ebc35799e4d94
lary18 <134699306+lary18@users.noreply.github.com>
2023-09-29 10:46:25 -0300

    Add files via upload

--- aluramidi,lary/main.js
+++ aluramidi,lary/main.js
@@ -1,2 +1,18 @@
-document.querySelector('.tecla_pom')
-document.querySelector('#som_tecla_pom').play();
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play(); 
+}
+
+document.querySelector('.tecla_pom').onclik = tocaSomPom;
+
+
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play(); 
+}
+
+document.querySelector('.tecla_pom').onclik = tocaSomClap;
+
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play(); 
+}
+
+document.querySelector('.tecla_pom').onclik = tocaSomTim;
